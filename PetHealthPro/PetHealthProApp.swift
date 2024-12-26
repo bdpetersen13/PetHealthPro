6ced57f0850e1080ef4ebf4c73ee9c9e9a52755d
Brandon Petersen <89234922+bdpetersen13@users.noreply.github.com>
2024-12-26 09:27:03 -0600

    Initial Commit

---
+++ PetHealthPro/PetHealthProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetHealthProApp.swift
+//  PetHealthPro
+//
+//  Created by Brandon Petersen on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct PetHealthProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
